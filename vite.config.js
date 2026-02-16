affd3e4945fc520cfd85a70b0a380c9493a50db7
KarlyleFr <karlyle.fr@gmail.com>
2026-02-16 21:07:08 +0000

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For GitHub Pages compatibility
+  base: '/MyPortfolio/', // For GitHub Pages compatibility
 })
+
